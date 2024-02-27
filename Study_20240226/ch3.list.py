8ee2a3962fad34b09816654e13613611326c0939
RANNY1042 <hyeeran1042@gmail.com>
2024-02-27 14:16:22 +0900

    ch4

--- Study_20240226/ch3.list.py
+++ Study_20240226/ch3.list.py
@@ -17,7 +17,20 @@ print("\nHere is the original list again:")
 print(cars)
 
 
-travle = [England, America, philiphine, spain, boracay]
+travle = ["England", "America", "philiphine", "spain", "boracay"]
 print(travle)
 print(sorted(travle))
 
+travle.reverse()
+print(travle)
+print(travle)
+
+travle.sort()
+print(travle)
+
+print(travle)
+
+travle.sort(reverse=True)
+print(travle)
+
+print(travle)
